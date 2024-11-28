8d2b5712784faa030e1f8e8877d5a28339cc1fe6
dhivyarrk <dhivyabharathi.radhakrishnanr@gmail.com>
2024-11-28 18:32:46 +0200

    Add landing page for frontend

---
+++ frontendapp/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+
+export const routes: Routes = [
+    { path: '', component: LandingpageComponent }, // Default route
+];
